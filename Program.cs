0cfc181a07fa0a2b781c87922786418a89836244
Serega5555 <lazarpalace@gmail.com>
2025-07-30 14:31:20 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = (from classroom in classes
+                         from student in classroom.Students
+                         select student
+                         .ToString())
+                         .ToArray();
+
+            return result;
+        }
+    }
+}
